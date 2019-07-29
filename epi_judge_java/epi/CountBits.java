d9e4ba49fc75fb9e3e43a9fa0802214235cf6989
Fidek Pavol <dafidack123@gmail.com>
2019-07-29 23:38:16 +0200

    Count bits
    Create method for counting bits in word

--- epi_judge_java/epi/CountBits.java
+++ epi_judge_java/epi/CountBits.java
@@ -1,12 +1,25 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class CountBits {
   @EpiTest(testDataFile = "count_bits.tsv")
 
   public static short countBits(int x) {
-    // TODO - you fill in here.
-    return 0;
+    short numOfBits = 0;
+
+    while (x != 0) {
+      numOfBits += (x & 1); // Add to numOfBits the result of (x AND 1), check first bit of the word
+                            // 00001001
+                            // 00000001
+                            // --------
+                            // 00000001 => then the line of code increment numOfBits variable, else the variable will not change
+
+      x >>>= 1; // Righ shift X (00001001 >>> 1 => 00000100)
+    }
+
+    return numOfBits;
   }
 
   public static void main(String[] args) {

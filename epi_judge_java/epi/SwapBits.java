0737bc926da71c24cb166dfe1346fa372bf726d9
Fidek Pavol <dafidack123@gmail.com>
2019-07-30 23:24:38 +0200

    Swap bits
    Create method for swaping bits on given indexes

--- epi_judge_java/epi/SwapBits.java
+++ epi_judge_java/epi/SwapBits.java
@@ -1,11 +1,18 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class SwapBits {
+
   @EpiTest(testDataFile = "swap_bits.tsv")
   public static long swapBits(long x, int i, int j) {
-    // TODO - you fill in here.
-    return 0;
+    if (((x >>> i) & 1) != ((x >>> j) & 1)) {    // Check if the swapped bit are the same, if bits are the same method just return the original 'x'
+      long bitMask = (1L << i) | (1L << j);      // Create bit mask for swap bits, e.g. 00000010 | 01000000 => 01000010 (bit mask)
+      x ^= bitMask;                              // Applay bit mask to x, XOR operator (^) will swap the bits on given indexes
+    }
+
+    return x;
   }
 
   public static void main(String[] args) {

98d9d2902de2b8c61d81c1c238114739ad0fde6e
Fidek Pavol <dafidack123@gmail.com>
2019-07-30 20:40:47 +0200

    Parity
    Counting the parity of a binary word

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -1,11 +1,23 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class Parity {
   @EpiTest(testDataFile = "parity.tsv")
+
   public static short parity(long x) {
-    // TODO - you fill in here.
-    return 0;
+    short result = 0;
+
+    while (x != 0) {
+      result ^= 1; // Counting the parity (result will be only 0 - even, 1 - odd)
+                   // e.g. 0001 ^ 0001 => 0000, 0000 ^ 0001 => 0001
+
+      x &= (x - 1); // Clear the lowest bit in word
+                   // e.g. 0110 (6) & 0101 (5) => 0100 (4)
+    }
+
+    return result;
   }
 
   public static void main(String[] args) {

56652dbab6a0385d4f6fe26b42042d1e71f6de86
Fidek Pavol <dafidack123@gmail.com>
2019-07-31 23:58:42 +0200

    Closest int same weight

--- epi_judge_java/epi/ClosestIntSameWeight.java
+++ epi_judge_java/epi/ClosestIntSameWeight.java
@@ -1,11 +1,24 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class ClosestIntSameWeight {
+
   @EpiTest(testDataFile = "closest_int_same_weight.tsv")
   public static long closestIntSameBitCount(long x) {
-    // TODO - you fill in here.
-    return 0;
+    final int NUM_UNSIGNED_BITS = 63;
+
+    for (int i = 0; i < NUM_UNSIGNED_BITS - 1; i++) {
+      if (((x >>> i) & 1) != ((x >>> i + 1) & 1)) { // Check the pair of bits if are the same
+        // x = SwapBits.swapBits(x, i, i + 1);
+        x ^= (1L << i) | (1L << (i + 1)); // Swap bits
+
+        return x;
+      }
+    }
+
+    throw new IllegalArgumentException("All bits are 0 or 1");
   }
 
   public static void main(String[] args) {
